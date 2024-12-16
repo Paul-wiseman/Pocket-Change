20a7f401f3ab25a4054262c936e493c10263062a
Paul Ndibe <paulndibe92@gmail.com>
2024-12-16 23:24:39 +0000

    Added documentation to the CurrencyExchangeData class

--- app/src/main/java/com/wiseman/currencyconverter/presentation/state/CurrencyExchangeData.kt
+++ app/src/main/java/com/wiseman/currencyconverter/presentation/state/CurrencyExchangeData.kt
@@ -2,6 +2,14 @@ package com.wiseman.currencyconverter.presentation.state
 
 import com.wiseman.currencyconverter.util.Constants.ZERO
 
+/**
+ * Data class representing currency exchange information.
+ *
+ * @property sellingCurrency The currency being sold. Defaults to EUR with amount 0.
+ * @property buyingCurrency The currency being bought. Defaults to USD with amount 0.
+ * @property commission The commission applied to the exchange. Defaults to 0.
+ * @property totalAmount The total amount of the transaction in the selling currency. Defaults to 0.
+ */
 data class CurrencyExchangeData(
     val sellingCurrency: Currency = Currency("EUR", ZERO),
     val buyingCurrency: Currency = Currency("USD", ZERO),
